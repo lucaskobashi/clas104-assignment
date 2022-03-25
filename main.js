09b13d39bf852ef46f8d899cd7953929a2a773d0
lucas kobashi <lucas.kobashi@cs.usask.ca>
2022-03-24 18:00:07 -0600

    trying to change directory for html pages:

--- main.js
+++ main.js
@@ -2,16 +2,13 @@
 
 $(document).ready(function () {
 
-    if (jQuery) {
-        alert("it's working!");}
-
     $("#home").click(function(){
         $('#loadProject').empty();
-        $("#loadHome").load("/src/home.html");
+        $("#loadHome").load("home.html");
     })
 
     $("#project").click(function(){
         $('#loadHome').empty();
-        $("#loadProject").load("/src/project.html");
+        $("#loadProject").load("project.html");
     })
 });
